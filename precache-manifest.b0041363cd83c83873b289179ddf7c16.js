b5cd2b7143a09e3aa69c5d4f9c6b2c4a27dbb95c
hanumankumarp <hanumankumar035@gmail.com>
2019-11-28 14:45:58 +0530

    Updates

---
+++ precache-manifest.b0041363cd83c83873b289179ddf7c16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9419c146ff264a82c8f7b5a5a6f677",
+    "url": "/sampleReactApp/index.html"
+  },
+  {
+    "revision": "2b1a410d7678cf83c8f5",
+    "url": "/sampleReactApp/static/css/main.5c9fd624.chunk.css"
+  },
+  {
+    "revision": "d78065286ff9bc99211d",
+    "url": "/sampleReactApp/static/js/2.b8a83d7c.chunk.js"
+  },
+  {
+    "revision": "2b1a410d7678cf83c8f5",
+    "url": "/sampleReactApp/static/js/main.1f5c431a.chunk.js"
+  },
+  {
+    "revision": "38d98ddb6d43f190de3b",
+    "url": "/sampleReactApp/static/js/runtime-main.d48c9fda.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sampleReactApp/static/media/logo.25bf045c.svg"
+  }
+]);
